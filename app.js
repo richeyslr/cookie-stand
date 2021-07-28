9d6cc9689e7f9d93f2d245d1fb7aab732229b14c
Spencer Richey <richeyslr@gmail.com>
2021-07-27 18:57:01 -0700

    JS works and renders sales numbers onto html page. No styling yet. Big W.

--- app.js
+++ app.js
@@ -4,72 +4,226 @@ const location1 = {
   minAvgCust: 23,
   maxAvgCust: 65,
   avgCookiesPerSale: 6.3,
+  randHourlyCustomers: [],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+  totalCookiesInDay: function(){
+    for (let i = 0; i < this.cookiesSoldPerHour.length; i++){
+      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
+  },
+
   // returns a random number of customers between the max and min
   randCustPerHour: function (){
-    return Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust);
+    for ( let i = 0; i < this.hoursOpen.length; i++){
+    this.randHourlyCustomers.push(Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust));
+    }
   },
-  // returns the random number of customers times average cookies sold per customer
+  // pushes the random number of customers times average cookies sold per customer to new array and adds
   randCookieSoldPerHr: function(){
-    return Math.floor(this.randCustPerHour() * this.avgCookiesPerSale);
-  }
+    for (let i = 0; i < this.hoursOpen.length; i++){
+    this.cookiesSoldPerHour.push(Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale));
+    this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
+  },
+  render() {
+    this.randCookieSoldPerHr();
+    const unorderedList = document.getElementById('location-1-list');
+    for (let i = 0; i < this.hoursOpen.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = this.hoursOpen[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Total: ' + this.totalCookiesPerDay + ' cookies';
+    unorderedList.appendChild(listItem);
+  },
+  hoursOpen: ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm"]
 }
 const location2 = {
   cityName: "Tokyo",
   minAvgCust: 3,
   maxAvgCust: 24,
   avgCookiesPerSale: 1.2,
-  randCustPerHour: function (){
-    return Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust);
+  randHourlyCustomers: [],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+  totalCookiesInDay: function(){
+    for (let i = 0; i < this.cookiesSoldPerHour.length; i++){
+      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
   },
+
+  // returns a random number of customers between the max and min
+  randCustPerHour: function (){
+    for ( let i = 0; i < this.hoursOpen.length; i++){
+    this.randHourlyCustomers.push(Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust));
+    }
+  },
+  // pushes the random number of customers times average cookies sold per customer to new array and adds
   randCookieSoldPerHr: function(){
-    return Math.floor(this.randCustPerHour() * this.avgCookiesPerSale);
-  }
+    for (let i = 0; i < this.hoursOpen.length; i++){
+    this.cookiesSoldPerHour.push(Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale));
+    this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
+  },
+  render() {
+    this.randCookieSoldPerHr();
+    const unorderedList = document.getElementById('location-2-list');
+    for (let i = 0; i < this.hoursOpen.length; i++) {
+        const listItem = document.createElement('li');
+        // 6am: 16 cookies
+        listItem.textContent = this.hoursOpen[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Total: ' + this.totalCookiesPerDay + ' cookies';
+    unorderedList.appendChild(listItem);
+  },
+  hoursOpen: ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm"]
 }
 const location3 = {
   cityName: "Dubai",
   minAvgCust: 11,
   maxAvgCust: 38,
   avgCookiesPerSale: 3.7,
-  randCustPerHour: function (){
-    return Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust);
+  randHourlyCustomers: [],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+  totalCookiesInDay: function(){
+    for (let i = 0; i < this.cookiesSoldPerHour.length; i++){
+      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
   },
+
+  // returns a random number of customers between the max and min
+  randCustPerHour: function (){
+    for ( let i = 0; i < this.hoursOpen.length; i++){
+    this.randHourlyCustomers.push(Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust));
+    }
+  },
+  // pushes the random number of customers times average cookies sold per customer to new array and adds
   randCookieSoldPerHr: function(){
-    return Math.floor(this.randCustPerHour() * this.avgCookiesPerSale);
-  }
+    for (let i = 0; i < this.hoursOpen.length; i++){
+    this.cookiesSoldPerHour.push(Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale));
+    this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
+  },
+  render() {
+    this.randCookieSoldPerHr();
+    const unorderedList = document.getElementById('location-3-list');
+    for (let i = 0; i < this.hoursOpen.length; i++) {
+        const listItem = document.createElement('li');
+        // 6am: 16 cookies
+        listItem.textContent = this.hoursOpen[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Total: ' + this.totalCookiesPerDay + ' cookies';
+    unorderedList.appendChild(listItem);
+  },
+  hoursOpen: ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm"]
 }
 const location4 = {
   cityName: "Paris",
   minAvgCust: 20,
   maxAvgCust: 38,
   avgCookiesPerSale: 2.3,
-  randCustPerHour: function (){
-    return Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust);
+  randHourlyCustomers: [],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+  totalCookiesInDay: function(){
+    for (let i = 0; i < this.cookiesSoldPerHour.length; i++){
+      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
   },
+
+  // returns a random number of customers between the max and min
+  randCustPerHour: function (){
+    for ( let i = 0; i < this.hoursOpen.length; i++){
+    this.randHourlyCustomers.push(Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust));
+    }
+  },
+  // pushes the random number of customers times average cookies sold per customer to new array and adds
   randCookieSoldPerHr: function(){
-    return Math.floor(this.randCustPerHour() * this.avgCookiesPerSale);
-  }
+    for (let i = 0; i < this.hoursOpen.length; i++){
+    this.cookiesSoldPerHour.push(Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale));
+    this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
+  },
+  render() {
+    this.randCookieSoldPerHr();
+    const unorderedList = document.getElementById('location-4-list');
+    for (let i = 0; i < this.hoursOpen.length; i++) {
+        const listItem = document.createElement('li');
+        // 6am: 16 cookies
+        listItem.textContent = this.hoursOpen[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Total: ' + this.totalCookiesPerDay + ' cookies';
+    unorderedList.appendChild(listItem);
+  },
+  hoursOpen: ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm"]
 }
 const location5 = {
   cityName: "Lima",
   minAvgCust: 2,
   maxAvgCust: 16,
   avgCookiesPerSale: 4.6,
-  randCustPerHour: function (){
-    return Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust);
+  randHourlyCustomers: [],
+  cookiesSoldPerHour: [],
+  totalCookiesPerDay: 0,
+  totalCookiesInDay: function(){
+    for (let i = 0; i < this.cookiesSoldPerHour.length; i++){
+      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
   },
+
+  // returns a random number of customers between the max and min
+  randCustPerHour: function (){
+    for ( let i = 0; i < this.hoursOpen.length; i++){
+    this.randHourlyCustomers.push(Math.floor(Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust));
+    }
+  },
+  // pushes the random number of customers times average cookies sold per customer to new array and adds
   randCookieSoldPerHr: function(){
-    return Math.floor(this.randCustPerHour() * this.avgCookiesPerSale);
-  }
+    for (let i = 0; i < this.hoursOpen.length; i++){
+    this.cookiesSoldPerHour.push(Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale));
+    this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+    }
+  },
+  render() {
+    this.randCookieSoldPerHr();
+    const unorderedList = document.getElementById('location-5-list');
+    for (let i = 0; i < this.hoursOpen.length; i++) {
+        const listItem = document.createElement('li');
+        // 6am: 16 cookies
+        listItem.textContent = this.hoursOpen[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Total: ' + this.totalCookiesPerDay + ' cookies';
+    unorderedList.appendChild(listItem);
+  },
+  hoursOpen: ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm"]
 }
 // create an array for all locations
 const storeLocations = [location1, location2, location3, location4, location5];
+for (let i = 0; i < storeLocations.length; i++){
+storeLocations[i].randCustPerHour();
+storeLocations[i].randCookieSoldPerHr();
+}
+console.log(`random customers per hour: ${location1.randHourlyCustomers}`);
+console.log(`cookies sold per hour: ${location1.cookiesSoldPerHour}`);
+console.log(`total daily cookies sold: ${location1.totalCookiesPerDay}`);
+for (let i = 0; i < storeLocations.length; i++){
+  storeLocations[i].render();
+}
+
 
 // print values to sales.html page
-for (i = 0; i < storeLocations.length; i++){
-  const cityHeader = document.createElement("h1");
-  cityHeader.innerText = storeLocations[i].cityName;
-  console.log(storeLocations[i].cityName);
-}
+
 
 
 
