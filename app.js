903045d646b2bd7e3c764790052f9936b9334d12
Spencer Richey <richeyslr@gmail.com>
2021-07-29 17:37:29 -0700

    used constructor to build objects for each location with prototype methods.

--- app.js
+++ app.js
@@ -1,364 +1,84 @@
-// creating objects for each location
-const location1 = {
-  cityName: "Seattle",
-  minAvgCust: 23,
-  maxAvgCust: 65,
-  avgCookiesPerSale: 6.3,
-  randHourlyCustomers: [], // stores random number of hourly customer
-  cookiesSoldPerHour: [], // stores cookies sold per hour by multiplying random hourly customer by avg cookies per sale
-  totalCookiesPerDay: 0,
-  // pushes a random number of customers between the max and min to random hourly customers array
-  randCustPerHour: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.randHourlyCustomers.push(
-        Math.floor(
-          Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
-        )
-      );
-    }
-  },
-  // pushes the random number of customers times average cookies sold per customer to new array and adds
-  randCookieSoldPerHr: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.cookiesSoldPerHour.push(
-        Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
-      );
-      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
-    }
-  },
-  render() {
-    // run the function to populate random cookies sold array
-    this.randCookieSoldPerHr();
-    this.getLocationTableData();
-    // get table from DOM
-    const table = document.getElementById("table");
-    // insert a new row in the table for the hours open for each store
-    // let times = table.insertRow();
-    // // loop through each hour open in hours open array to create a cell and print the value in each new cell
-    // for (let i = 0; i < this.hoursOpen.length; i++) {
-    //   let timeCells = times.insertCell();
-    //   timeCells.textContent = this.hoursOpen[i];
-    // }
-    const locationData = table.insertRow();
-    for (let i = 0; i < this.locationTableData.length; i++) {
-      let dataCells = locationData.insertCell();
-      dataCells.textContent = this.locationTableData[i];
-    }
-  },
-  hoursOpen: [
-    "6am",
-    "7am",
-    "8am",
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-    "6pm",
-    "7pm",
-  ],
-  locationTableData: [],
-  getLocationTableData: function () {
-    this.locationTableData.push(this.cityName);
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
-      this.locationTableData.push(`${this.cookiesSoldPerHour[i]} cookies`);
-    }
-    this.locationTableData.push(`${this.totalCookiesPerDay} cookies`);
-  },
-};
-const location2 = {
-  cityName: "Tokyo",
-  minAvgCust: 3,
-  maxAvgCust: 24,
-  avgCookiesPerSale: 1.2,
-  randHourlyCustomers: [],
-  cookiesSoldPerHour: [],
-  totalCookiesPerDay: 0,
-  // returns a random number of customers between the max and min
-  randCustPerHour: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.randHourlyCustomers.push(
-        Math.floor(
-          Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
-        )
-      );
-    }
-  },
-  // pushes the random number of customers times average cookies sold per customer to new array and adds
-  randCookieSoldPerHr: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.cookiesSoldPerHour.push(
-        Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
-      );
-      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
-    }
-  },
-  render() {
-    // run the function to populate random cookies sold array
-    this.randCookieSoldPerHr();
-    this.getLocationTableData();
-    // get table from DOM
-    const table = document.getElementById("table");
-    // insert a new row in the table for the hours open for each store
-    // let times = table.insertRow();
-    // // loop through each hour open in hours open array to create a cell and print the value in each new cell
-    // for (let i = 0; i < this.hoursOpen.length; i++) {
-    //   let timeCells = times.insertCell();
-    //   timeCells.textContent = this.hoursOpen[i];
-    // }
-    const locationData = table.insertRow();
-    for (let i = 0; i < this.locationTableData.length; i++) {
-      let dataCells = locationData.insertCell();
-      dataCells.textContent = this.locationTableData[i];
-    }
-  },
-  hoursOpen: [
-    "6am",
-    "7am",
-    "8am",
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-    "6pm",
-    "7pm",
-  ],
-  locationTableData: [],
-  getLocationTableData: function () {
-    this.locationTableData.push(this.cityName);
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
-      this.locationTableData.push(`${this.cookiesSoldPerHour[i]} cookies`);
-    }
-    this.locationTableData.push(`${this.totalCookiesPerDay} cookies`);
-  },
-};
-const location3 = {
-  cityName: "Dubai",
-  minAvgCust: 11,
-  maxAvgCust: 38,
-  avgCookiesPerSale: 3.7,
-  randHourlyCustomers: [],
-  cookiesSoldPerHour: [],
-  totalCookiesPerDay: 0,
-  // returns a random number of customers between the max and min
-  randCustPerHour: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.randHourlyCustomers.push(
-        Math.floor(
-          Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
-        )
-      );
-    }
-  },
-  // pushes the random number of customers times average cookies sold per customer to new array and adds
-  randCookieSoldPerHr: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.cookiesSoldPerHour.push(
-        Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
-      );
-      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
-    }
-  },
-  render() {
-    // run the function to populate random cookies sold array
-    this.randCookieSoldPerHr();
-    this.getLocationTableData();
-    // get table from DOM
-    const table = document.getElementById("table");
-    // insert a new row in the table for the hours open for each store
-    // let times = table.insertRow();
-    // // loop through each hour open in hours open array to create a cell and print the value in each new cell
-    // for (let i = 0; i < this.hoursOpen.length; i++) {
-    //   let timeCells = times.insertCell();
-    //   timeCells.textContent = this.hoursOpen[i];
-    // }
-    const locationData = table.insertRow();
-    for (let i = 0; i < this.locationTableData.length; i++) {
-      let dataCells = locationData.insertCell();
-      dataCells.textContent = this.locationTableData[i];
-    }
-  },
-  hoursOpen: [
-    "6am",
-    "7am",
-    "8am",
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-    "6pm",
-    "7pm",
-  ],
-  locationTableData: [],
-  getLocationTableData: function () {
-    this.locationTableData.push(this.cityName);
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
-      this.locationTableData.push(`${this.cookiesSoldPerHour[i]} cookies`);
-    }
-    this.locationTableData.push(`${this.totalCookiesPerDay} cookies`);
-  },
-};
-const location4 = {
-  cityName: "Paris",
-  minAvgCust: 20,
-  maxAvgCust: 38,
-  avgCookiesPerSale: 2.3,
-  randHourlyCustomers: [],
-  cookiesSoldPerHour: [],
-  totalCookiesPerDay: 0,
-  // returns a random number of customers between the max and min
-  randCustPerHour: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.randHourlyCustomers.push(
-        Math.floor(
-          Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
-        )
-      );
-    }
-  },
-  // pushes the random number of customers times average cookies sold per customer to new array and adds
-  randCookieSoldPerHr: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.cookiesSoldPerHour.push(
-        Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
-      );
-      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
-    }
-  },
-  render() {
-    // run the function to populate random cookies sold array
-    this.randCookieSoldPerHr();
-    this.getLocationTableData();
-    // get table from DOM
-    const table = document.getElementById("table");
-    // insert a new row in the table for the hours open for each store
-    // let times = table.insertRow();
-    // // loop through each hour open in hours open array to create a cell and print the value in each new cell
-    // for (let i = 0; i < this.hoursOpen.length; i++) {
-    //   let timeCells = times.insertCell();
-    //   timeCells.textContent = this.hoursOpen[i];
-    // }
-    const locationData = table.insertRow();
-    for (let i = 0; i < this.locationTableData.length; i++) {
-      let dataCells = locationData.insertCell();
-      dataCells.textContent = this.locationTableData[i];
-    }
-  },
-  hoursOpen: [
-    "6am",
-    "7am",
-    "8am",
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-    "6pm",
-    "7pm",
-  ],
-  locationTableData: [],
-  getLocationTableData: function () {
-    this.locationTableData.push(this.cityName);
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
-      this.locationTableData.push(`${this.cookiesSoldPerHour[i]} cookies`);
-    }
-    this.locationTableData.push(`${this.totalCookiesPerDay} cookies`);
-  },
-};
-const location5 = {
-  cityName: "Lima",
-  minAvgCust: 2,
-  maxAvgCust: 16,
-  avgCookiesPerSale: 4.6,
-  randHourlyCustomers: [],
-  cookiesSoldPerHour: [],
-  totalCookiesPerDay: 0,
-  // returns a random number of customers between the max and min
-  randCustPerHour: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.randHourlyCustomers.push(
-        Math.floor(
-          Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
-        )
-      );
-    }
-  },
-  // pushes the random number of customers times average cookies sold per customer to new array and adds
-  randCookieSoldPerHr: function () {
-    for (let i = 0; i < this.hoursOpen.length; i++) {
-      this.cookiesSoldPerHour.push(
-        Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
-      );
-      this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
-    }
-  },
-  // method for each location to render data to webpage
-  render() {
-    // run the function to populate random cookies sold array
-    this.randCookieSoldPerHr();
-    this.getLocationTableData();
-    // get table from DOM
-    const table = document.getElementById("table");
-    // insert a new row in the table for the hours open for each store
-    // let times = table.insertRow();
-    // // loop through each hour open in hours open array to create a cell and print the value in each new cell
-    // for (let i = 0; i < this.hoursOpen.length; i++) {
-    //   let timeCells = times.insertTH();
-    //   timeCells.textContent = this.hoursOpen[i];
-    // }
-    const locationData = table.insertRow();
-    for (let i = 0; i < this.locationTableData.length; i++) {
-      let dataCells = locationData.insertCell();
-      dataCells.textContent = this.locationTableData[i];
-    }
-  },
-  // array of hours open
-  hoursOpen: [
-    "6am",
-    "7am",
-    "8am",
-    "9am",
-    "10am",
-    "11am",
-    "12pm",
-    "1pm",
-    "2pm",
-    "3pm",
-    "4pm",
-    "5pm",
-    "6pm",
-    "7pm",
-  ],
-  locationTableData: [],
-  getLocationTableData: function () {
-    this.locationTableData.push(this.cityName);
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
-      this.locationTableData.push(`${this.cookiesSoldPerHour[i]} cookies`);
-    }
-    this.locationTableData.push(`${this.totalCookiesPerDay} cookies`);
-  },
-};
-// create an array for all locations
-const storeLocations = [location1, location2, location3, location4, location5];
+// trying to use constructor to build store locations
 
+// defining hours open outside of objects
+let hoursOpen = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
+
+// creating new class named Location to generate coookie stand locations. passing in city names and customer/sales data
+function Location(shopCity, minValue, maxValue, avgCookiesPerSaleValue) {
+  this.cityName = shopCity;
+  this.minAvgCust = minValue;
+  this.maxAvgCust = maxValue;
+  this.avgCookiesPerSale = avgCookiesPerSaleValue;
+  // defining empty arrays and totals counter
+  this.randHourlyCustomers = [];
+  this.cookiesSoldPerHour = [];
+  this.totalCookiesPerDay = 0;
+  this.hours = hoursOpen;
+  this.locationTableData = [];
+}
+
+// defining Location class methods using same code as previous labs
+
+Location.prototype.randCookieSoldPerHr = function () {
+  for (let i = 0; i < hoursOpen.length; i++) {
+    this.cookiesSoldPerHour.push(
+      Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
+    );
+    this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
+  }
+};
+Location.prototype.randCustPerHour = function () {
+  for (let i = 0; i < hoursOpen.length; i++) {
+    this.randHourlyCustomers.push(
+      Math.floor(
+        Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
+      )
+    );
+  }
+};
+Location.prototype.getLocationTableData = function () {
+  this.locationTableData.push(this.cityName);
+  for (let i = 0; i < this.cookiesSoldPerHour.length; i++) {
+    this.locationTableData.push(`${this.cookiesSoldPerHour[i]} cookies`);
+  }
+  this.locationTableData.push(`${this.totalCookiesPerDay} cookies`);
+};
+Location.prototype.render = function () {
+  this.randCookieSoldPerHr();
+  this.getLocationTableData();
+  // get table from DOM
+  const table = document.getElementById("table");
+  // insert a new row in the table for the hours open for each store
+  const locationData = table.insertRow();
+  for (let i = 0; i < this.locationTableData.length; i++) {
+    let dataCells = locationData.insertCell();
+    dataCells.textContent = this.locationTableData[i];
+  }
+};
+
+// creating new 
+let location1 = new Location("Seattle", 23, 65, 2.3);
+let location2 = new Location("Tokyo", 3, 24, 1.2);
+let location3 = new Location("Dubai", 11, 38, 3.7);
+let location4 = new Location("Paris", 20, 38, 2.3);
+let location5 = new Location("Lima", 2, 16, 4.6);
+console.log(location1.cityName);
+const storeLocations = [location1, location2, location3, location4, location5];
 
 for (let i = 0; i < storeLocations.length; i++) {
   storeLocations[i].randCustPerHour();
@@ -370,71 +90,16 @@ for (let i = 0; i < storeLocations.length; i++) {
   storeLocations[i].render();
 }
 
-// create a function to render the table's headings
+const totalsRow = [" "];
 
-// trying to use constructor to build store locations
-
-// function Location(storeLocation, minValue, maxValue, avgCookiesPerSaleValue){
-//   let storeLocation = this.name;
-//   let minValue = this.minAvgCust;
-//   let maxValue = this.maxAvgCust;
-//   let avgCookiesPerSaleValue = this.avgCookiesPerSale;
-//   let randHourlyCustomers = [];
-//   let cookiesSoldPerHour = [];
-//   let totalCookiesPerDay = 0;
-//   // returns a random number of customers between the max and min
-//   this.randCustPerHour() = function (){
-//     for (let i = 0; i < this.hoursOpen.length; i++) {
-//       this.randHourlyCustomers.push(
-//         Math.floor(
-//           Math.random() * (this.maxAvgCust - this.minAvgCust) + this.minAvgCust
-//         )
-//       );
-//     }
-//   };
-//   // pushes the random number of customers times average cookies sold per customer to new array and adds
-//   this.randCookieSoldPerHr = function () {
-//     for (let i = 0; i < this.hoursOpen.length; i++) {
-//       this.cookiesSoldPerHour.push(
-//         Math.floor(this.randHourlyCustomers[i] * this.avgCookiesPerSale)
-//       );
-//       this.totalCookiesPerDay += this.cookiesSoldPerHour[i];
-//     }
-//   };
-//   // method for each location to render data to webpage
-//   this.render() = function() {
-//     this.randCookieSoldPerHr();
-//     const unorderedList = document.getElementById("location-5-list");
-//     for (let i = 0; i < this.hoursOpen.length; i++) {
-//       const listItem = document.createElement("li");
-//       // 6am: 16 cookies
-//       listItem.textContent =
-//         this.hoursOpen[i] + ": " + this.cookiesSoldPerHour[i] + " cookies";
-//       unorderedList.appendChild(listItem);
-//     }
-//     const listItem = document.createElement("li");
-//     listItem.textContent = "Total: " + this.totalCookiesPerDay + " cookies";
-//     unorderedList.appendChild(listItem);
-//   };
-//   // array of hours open
-//   this.hoursOpen = [
-//     "6am",
-//     "7am",
-//     "8am",
-//     "9am",
-//     "10am",
-//     "11am",
-//     "12pm",
-//     "1pm",
-//     "2pm",
-//     "3pm",
-//     "4pm",
-//     "5pm",
-//     "6pm",
-//     "7pm",
-//   ];
-
-// };
-
-// const location1 = new Location("Seattle",23,65,6.3);
-// console.log(location1.name);
+function getTotalsRow()
+{
+  // add all locations' 1st values in cookies sold per hour array
+  for (let i = 0; i < location1.cookiesSoldPerHour.length; i++) 
+  {
+    let totals = (location1.cookiesSoldPerHour[i] + location2.cookiesSoldPerHour[i] + location3.cookiesSoldPerHour[i] + location4.cookiesSoldPerHour + location5.cookiesSoldPerHour[i])
+  totalsRow.push(totals);
+  }
+}
+getTotalsRow();
+console.log(totalsRow);
